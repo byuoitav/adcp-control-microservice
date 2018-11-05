3aca02de12099e1c260baf395a1e06bc5b6a8909
Joseph Blodgett <joseph.blodgett@gmail.com>
2018-11-05 10:12:36 -0700

    adding status endpoint

--- server.go
+++ server.go
@@ -5,24 +5,19 @@ import (
 
 	"github.com/byuoitav/adcp-control-microservice/handlers"
 	"github.com/byuoitav/authmiddleware"
+	"github.com/byuoitav/common"
 	"github.com/byuoitav/common/log"
 	"github.com/byuoitav/hateoas"
-	"github.com/jessemillar/health"
 	"github.com/labstack/echo"
-	"github.com/labstack/echo/middleware"
 )
 
 func main() {
 	port := ":8012"
-	router := echo.New()
-	router.Pre(middleware.RemoveTrailingSlash())
-	router.Use(middleware.CORS())
-
+	router := common.NewRouter()
 	// Use the `secure` routing group to require authentication
 	secure := router.Group("", echo.WrapMiddleware(authmiddleware.Authenticate))
 
 	router.GET("/", echo.WrapHandler(http.HandlerFunc(hateoas.RootResponse)))
-	router.GET("/health", echo.WrapHandler(http.HandlerFunc(health.Check)))
 
 	secure.GET("/:address/volume/set/:level", handlers.SetVolume)
 	secure.GET("/:address/power/on", handlers.PowerOn)
